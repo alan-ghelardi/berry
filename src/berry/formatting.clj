0b8527f3ea53245da11e67fe2dfda0d3cc627878
Alan Ghelardi <alan.ghelardi@gmail.com>
2016-11-08 22:28:06 -0200

    Create functions for handling indentation.

---
+++ src/berry/formatting.clj
@@ -0,0 +1,16 @@
+(ns berry.formatting)
+
+(defn- indent-with
+[character width]
+  (apply str (repeat width character)))
+
+(def tab (partial indent-with "\t"))
+
+(def space (partial indent-with " "))
+
+(defn indent
+[text {apply-indentation :indent-style
+  indent-size :indent-size
+  level :level}]
+(let [width (* indent-size level)] 
+    (str (apply-indentation width) text)))
